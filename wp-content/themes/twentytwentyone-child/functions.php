953d436b602cb75ece2588d000e3fece94bfe5d8
kavitapatel21 <kavita.patel@plutustec.com>
2023-02-02 17:21:19 +0530

    first

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -101,13 +101,13 @@ add_filter('woocommerce_account_menu_items', 'bbloomer_add_order_tracking_link_m
 function bbloomer_order_tracking_content()
 {
 
-    
-    defined( 'ABSPATH' ) || exit;
+
+    defined('ABSPATH') || exit;
 
     global $woocommerce, $user_id;
 
     if (!class_exists('WooCommerce') || !get_current_user_id()) {
-    return;
+        return;
     };
 
     $user_id = get_current_user_id();
@@ -116,13 +116,13 @@ function bbloomer_order_tracking_content()
     $posts_per_page = 20;
     // Get all customer orders
     $customer__all_orders = get_posts(apply_filters('woocommerce_my_account_my_orders_query', array(
-    'numberposts' => -1,
-    'meta_key' => '_customer_user',
-    'orderby' => 'date',
-    'order' => 'DESC',
-    'meta_value' => $user_id ,
-    'post_type' => wc_get_order_types(),
-    'post_status' => array_keys(wc_get_order_statuses()), 'post_status' => array('wc-processing','wc-completed','wc-on-hold','wc-arrival-shipment'),
+        'numberposts' => -1,
+        'meta_key' => '_customer_user',
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'meta_value' => $user_id,
+        'post_type' => wc_get_order_types(),
+        'post_status' => array_keys(wc_get_order_statuses()), 'post_status' => array('wc-processing', 'wc-completed', 'wc-on-hold', 'wc-arrival-shipment'),
     )));
     $paged = isset($_REQUEST['order_page']) ? $_REQUEST['order_page'] : 1;
     $total_records = count($customer__all_orders);
@@ -130,15 +130,15 @@ function bbloomer_order_tracking_content()
 
 
     $customer_orders = get_posts(array(
-    'meta_key' => '_customer_user',
-    'order' => 'DESC',
-    'meta_value' => $user_id ,
-    'post_type' => wc_get_order_types(),
-    'posts_per_page' => $posts_per_page,
-    'paged' => $paged,
-    'post_status' => array_keys(wc_get_order_statuses()), 'post_status' => array('wc-processing','wc-completed','wc-on-hold','wc-arrival-shipment'),
+        'meta_key' => '_customer_user',
+        'order' => 'DESC',
+        'meta_value' => $user_id,
+        'post_type' => wc_get_order_types(),
+        'posts_per_page' => $posts_per_page,
+        'paged' => $paged,
+        'post_status' => array_keys(wc_get_order_statuses()), 'post_status' => array('wc-processing', 'wc-completed', 'wc-on-hold', 'wc-arrival-shipment'),
     ));
-    ?>
+?>
 
 
     <?php if (!empty($customer_orders)) : ?>
@@ -230,3 +230,154 @@ function bbloomer_order_tracking_content()
 }
 
 add_action('woocommerce_account_order-tracking_endpoint', 'bbloomer_order_tracking_content');
+
+
+add_action('woocommerce_order_details_before_order_table_items', 'misha_after_customer');
+
+function misha_after_customer($order)
+{
+    //echo "<pre>";
+    //print_r($order->status);
+?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <style>
+        .hh-grayBox {
+            background-color: #F8F8F8;
+            margin-bottom: 20px;
+            padding: 35px;
+            margin-top: 20px;
+        }
+
+        .pt45 {
+            padding-top: 45px;
+        }
+
+        .order-tracking {
+            text-align: center;
+            width: 33.33%;
+            position: relative;
+            display: block;
+        }
+
+        .order-tracking .is-complete {
+            display: block;
+            position: relative;
+            border-radius: 50%;
+            height: 30px;
+            width: 30px;
+            border: 0px solid #AFAFAF;
+            background-color: #f7be16;
+            margin: 0 auto;
+            transition: background 0.25s linear;
+            -webkit-transition: background 0.25s linear;
+            z-index: 2;
+        }
+
+        .order-tracking .is-complete:after {
+            display: block;
+            position: absolute;
+            content: '';
+            height: 14px;
+            width: 7px;
+            top: -2px;
+            bottom: 0;
+            left: 5px;
+            margin: auto 0;
+            border: 0px solid #AFAFAF;
+            border-width: 0px 2px 2px 0;
+            transform: rotate(45deg);
+            opacity: 0;
+        }
+
+        .order-tracking.completed .is-complete {
+            border-color: #27aa80;
+            border-width: 0px;
+            background-color: #27aa80;
+        }
+
+        .order-tracking.completed .is-complete:after {
+            border-color: #fff;
+            border-width: 0px 3px 3px 0;
+            width: 7px;
+            left: 11px;
+            opacity: 1;
+        }
+
+        .order-tracking p {
+            color: #A4A4A4;
+            font-size: 16px;
+            margin-top: 8px;
+            margin-bottom: 0;
+            line-height: 20px;
+        }
+
+        .order-tracking p span {
+            font-size: 14px;
+        }
+
+        .order-tracking.completed p {
+            color: #000;
+        }
+
+        .order-tracking::before {
+            content: '';
+            display: block;
+            height: 3px;
+            width: calc(100% - 40px);
+            background-color: #f7be16;
+            top: 13px;
+            position: absolute;
+            left: calc(-50% + 20px);
+            z-index: 0;
+        }
+
+        .order-tracking:first-child:before {
+            display: none;
+        }
+
+        /** .order-tracking.completed:before {
+            background-color: #27aa80;
+        }*/
+    </style>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-10 hh-grayBox pt45 pb20">
+                <div class="row justify-content-between">
+                    <div class="order-tracking completed ordered">
+                        <span class="ordered is-complete"></span>
+                        <p>Ordered</p>
+                    </div>
+                    <div class="order-tracking completed shipped">
+                        <span class="shipped is-complete"></span>
+                        <p>Shipped</p>
+                    </div>
+                    <div class="order-tracking completed delivered">
+                        <span class="delivered is-complete"></span>
+                        <p>Delivered</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        //alert("hello");
+        $(document).ready(function() {
+            var order_status = '<?php echo $order->status ?>';
+            if (order_status == 'processing' || order_status == 'on-hold') {
+                $(".order-tracking.completed.ordered .is-complete").css("background-color", "#f7be16");
+            }
+            if (order_status == 'arrival-shipment') {
+                $(".order-tracking.completed.ordered .is-complete").css("background-color", "#f7be16");
+                $(".order-tracking.completed.shipped .is-complete").css("background-color", "#f7be16");
+                $(".order-tracking.completed.shipped:before .is-complete").css("background-color", "#f7be16");
+            }
+            if (order_status == 'completed') {
+                $(".order-tracking.completed.ordered .is-complete").css("background-color", "#f7be16");
+                $(".order-tracking.completed.shipped .is-complete").css("background-color", "#f7be16");
+                $(".order-tracking.completed.completed .is-complete").css("background-color", "#f7be16");
+            }
+        });
+    </script>
+<?php
+}
